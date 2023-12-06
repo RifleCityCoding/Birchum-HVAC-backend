947033f7aa88c3f834bf662affa24f8e8fa0335f
RifleCityCoding <142933516+RifleCityCoding@users.noreply.github.com>
2023-12-06 11:21:53 -0500

    adding pivot table

--- app/models/customer_pivot.py
+++ app/models/customer_pivot.py
@@ -2,7 +2,7 @@ from app.db.base_class import Base
 from sqlalchemy import  Column, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
-class CustomerConnect:
+class CustomerConnect(Base):
     __tablename__ = "customer_pivot"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -10,6 +10,9 @@ class CustomerConnect:
     customer_id = ForeignKey("customer_info.id")
     receipt_id = ForeignKey("receipts.id")
 
-    user = relationship("User", back_populates="customer_connects")
-    customer = relationship("CustomerInfo", back_populates="customer_connects")
+    users = relationship("User", back_populates="customer_connects")
+    customer = relationship("CustomerInfoModel", back_populates="customer_connects")
     receipt = relationship("Receipts", back_populates="customer_connects")
+
+    user_primary_join = relationship("User", back_populates="customer_connects", primaryjoin="User.id == CustomerConnect.user_id")
+    customer_primary_join = relationship("CustomerInfoModel", back_populates="customer_connects", primaryjoin="CustomerInfoModel.id == CustomerConnect.customer_id")
